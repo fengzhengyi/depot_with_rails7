f346f127b081a736f67fb90c402ffcce93b72557
fengzhengyi <512369179@qq.com>
2022-05-15 07:54:44 +0800

    task d:create cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,21 @@
 class Product < ApplicationRecord
-  validates :title,:description,:image_url,presence: true
-  validates :price,numericality: {greater_than_or_equal_to: 0.01}
-  validates :title,uniqueness: true
-  validates :image_url,allow_blank: true ,format: {
+  has_many :line_items
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|png|jpg)\z}i,
     message: 'must be a URL for GIF, PNG, JPG image.'
   }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
