bf76a36515c8b018b445588c6d359e618ec08814
fengzhengyi <512369179@qq.com>
2022-05-16 22:54:55 +0800

    task e:a smarter cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: %i[ show edit update destroy ]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts or /carts.json
   def index
@@ -49,22 +50,29 @@ class CartsController < ApplicationController
 
   # DELETE /carts/1 or /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if session[:cart_id] == @cart.id
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to store_index_url, notice: "Your cart is currently empty" }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart
-      @cart = Cart.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def cart_params
-      params.fetch(:cart, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def cart_params
+    params.fetch(:cart, {})
+  end
+
+  def invalid_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to store_index_url, notice: 'Invalid cart'
+  end
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -23,12 +23,12 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-    @product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product:@product)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
+        format.html { redirect_to cart_url(@line_item.cart) }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -69,6 +69,6 @@ class LineItemsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def line_item_params
-    params.require(:line_item).permit(:product_id, :cart_id)
+    params.require(:line_item).permit(:product_id)
   end
 end
